561e3ce8e5afc44848ab82c7c541e60e1eafcc64
dtheran <desarrollador.its15@metgroupsas.com>
2024-01-06 16:02:00 -0500

    server component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,47 @@
-export default function Page() {
+import { Suspense } from 'react';
+import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { lusitana } from '../ui/fonts';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  console.log('🚀 ~ file: page.tsx:9 ~ Page ~ revenue:', revenue);
+  
+  const totalPaidInvoices = revenue.reduce(
+    (acc, curr) => acc + curr.revenue,
+    0,
+  );
+
+  console.log(revenue);
+
   return (
-    <div>
-      <h1>Contenido del dashboard</h1>
-    </div>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
   );
 }
